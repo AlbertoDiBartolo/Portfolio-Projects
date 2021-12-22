51aee7970e8b7e997cbc24ed676f6399cca86519
Alby <94834498+AlbertoDiBartolo@users.noreply.github.com>
2021-12-22 16:53:23 +0100

    Update Example001.sql

--- Example001.sql
+++ Example001.sql
@@ -19,8 +19,6 @@ ALTER TABLE [dbo].[Customer] ALTER COLUMN [CustomerEmail] NVARCHAR(320) NOT NULL
 ALTER TABLE [dbo].[Customer] ALTER COLUMN [CustomerPhoneNumber] VARCHAR(15) NOT NULL
 ALTER TABLE [dbo].[Customer] ADD UNIQUE   ([CustomerID], [CustomerEmail], [CustomerPhoneNumber])
 
-
-
 UPDATE [dbo].[Customer]
 SET CustomerID = RTRIM(LTRIM(CustomerID)),
 	CustomerFirstName = UPPER(REPLACE(RTRIM(LTRIM(CustomerFirstName)), '.', '')),
@@ -30,21 +28,44 @@ SET CustomerID = RTRIM(LTRIM(CustomerID)),
 
 
 /*
-	Find customers that havent made any purchase in >= 10, >= 20 or >= 30 months
+
+Manipulating data to create new tables
+
+	Find customers that havent made any purchase in >= 10, >= 20 or >= 30 months (data from OrdersInfo table)
 	Add the customerID to new table "EligibleForDiscount"
 	Store the proposed discount as a field in the table
 	- 12 months = 10% discount
 	- 20 months = 15% discount
 	- 30 months = 20% discount
+
 */
 
 
 
-SELECT	CustomerID,
-		CASE	WHEN MIN(DATEDIFF(MONTH, OrderDate, GETDATE())) >= 30 THEN 20
-				WHEN MIN(DATEDIFF(MONTH, OrderDate, GETDATE())) >= 20 THEN 15
-				WHEN MIN(DATEDIFF(MONTH, OrderDate, GETDATE())) >= 10 THEN 10
-		END AS DiscountPercentage
+SELECT CustomerID,
+	CASE	WHEN MIN(DATEDIFF(MONTH, OrderDate, GETDATE())) >= 30 THEN 20
+		WHEN MIN(DATEDIFF(MONTH, OrderDate, GETDATE())) >= 20 THEN 15
+		WHEN MIN(DATEDIFF(MONTH, OrderDate, GETDATE())) >= 10 THEN 10
+	END AS DiscountPercentage
 INTO EligibleForDiscount
 FROM OrdersInfo
 GROUP BY CustomerID
+
+
+
+/*
+
+	Find customers that have no recorded purchases
+	Add the customerID to new table "TerminateAccountAlert"
+	Add customerEmail to the table
+
+*/
+
+
+
+SELECT Customer.CustomerID, CustomerEmail
+INTO TerminateAccountAlert
+FROM Customer
+LEFT JOIN OrdersInfo
+ON Customer.CustomerID = OrdersInfo.CustomerID
+WHERE OrdersInfo.OrderID IS NULL
